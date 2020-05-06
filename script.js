d1d9a7f0772475923bb74fd58f7f0e97488d1236
lifen-joy <63543144+lifen-joy@users.noreply.github.com>
2020-05-06 13:50:58 +0100

    Version 0.21
    Change some formated, added skin tone shades and edits the CSS colours

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
 const firstNames = ["Hindu", "Muslim", "Christian", "Buddhist"];
 
-const secondNames = ["Female", "Male", "Non-binary"];
+const secondNames = ["Female", "Male", "Non-binary", "Female"];
 
 const secondAbility = ["Touch", "Sight", "Hearing", "Speech"];
 
 const secondSexuality = ["Heterosexual", "Bisexual", "Queer"];
 
-const secondAge = ["Child", "Young-Adult", "Adult", "Older"];
+const secondAge = [ "16- 24", "25-65", "65+"];
+
+const skintone = ["#f9cfae", "#dea674", "#804e31", "b87139"];
 
 
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
 const getRandomName = () => 
-  `someone of ${firstNames[getRandomNumber(firstNames.length)]} faith, is of a  ${secondNames[getRandomNumber(secondNames.length)]} gender, may have a lost of ${secondAbility[getRandomNumber(secondAbility.length)]}, is from a ${secondSexuality[getRandomNumber(secondSexuality.length)]} group and of a ${secondAge[getRandomNumber(secondAge.length)]} generation?`;
+  `someone of ${firstNames[getRandomNumber(firstNames.length)]} faith,
+   is of a  ${secondNames[getRandomNumber(secondNames.length)]} gender, 
+   may have a lost of ${secondAbility[getRandomNumber(secondAbility.length)]}, 
+   is from a ${secondSexuality[getRandomNumber(secondSexuality.length)]} 
+   group and of a ${secondAge[getRandomNumber(secondAge.length)]} generation
+   of a ${skintone[getRandomNumber(skintone.length)]} skintone`;
 
 const setRandomName = () => {
   document.getElementById('random-name').innerText = getRandomName();
 }
 
-document.getElementById('generate')
+document.getElementById('SHUFFLE')
   .addEventListener('click', setRandomName);
 
 setRandomName();
